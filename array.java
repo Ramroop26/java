ea5c135c3e2f28c7028812098c957ee7a8fa07c7
Ramroop26 <ramroop26io071@satiengg.in>
2025-05-30 18:30:41 +0530

    commit

---
+++ array.java
@@ -0,0 +1,40 @@
+
+public class array{
+    public static void main(String[] args) {
+
+
+        //Array=> collection of items of the same data type stored in contiguous memory locations
+
+        //to store multiple element of same data type
+        //array are stored in heap = object
+        //array- declaration, instantization, initialization, destruction
+
+        //declaration
+        int arr1[]; //C++ used hota hai
+        int[] arr2;
+        int []arr3;
+
+        // invalid way
+        // int arr[3];
+
+        //instance=> 
+        int [] arr4=new int[3];
+
+        //invalid way- 
+        // int[] arr=new int[];
+
+        // initialization-
+        int arr5[] = {1,2,3,4};
+        int arr[]=new int[]{1,2,3,4};
+
+        //invalid way
+        // int arr11[]=new int[7]{1,2,3,4,5,6,7};
+        //int arr[3]={1,2,3};
+
+        // Dynamic initialization=>
+
+
+
+
+}
+}
---
+++ array1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class array1 {
+    public static void main(String[] args) {
+
+        //Dynamic initialization 
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = obj.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the value of the array:");
+        for(int i=0; i<arr.length;i++){ //arr.length is property of array and arr.length() is method
+            arr[i] = obj.nextInt();
+        }
+        for(int arr1:arr){
+            System.out.println(arr1);
+        }
+
+    
+}
+}
