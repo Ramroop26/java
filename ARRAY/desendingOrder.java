2a78fb03586cca6cb360d965e495bee54ccf0a86
Ramroop26 <ramroop26io071@satiengg.in>
2025-06-05 15:24:34 +0530

    commit

--- ARRAY/desendingOrder.java
+++ ARRAY/desendingOrder.java
@@ -1,16 +1,16 @@
 public class desendingOrder {
       public static void main(String[] args) {
-        int[] arr = {6,4,7,8,9,1};
-        for(int i=0; i<6; i++){
-            for(int j=i+1; j<6; j++){
-                if(arr[i]<arr[j]){
+        int[] arr = {15,20,20,10};
+        for(int i=0; i<4; i++){
+            for(int j=i+1; j<4; j++){
+                if(arr[j]>arr[i]){
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
-        for(int i=0; i<6; i++){
+        for(int i=0; i<4; i++){
             System.out.print(arr[i] + "");
         }
         
