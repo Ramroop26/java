012855ffa1944d022a61d495ee22a4a2385678d0
Ramroop26 <ramroop26io071@satiengg.in>
2025-06-19 22:49:48 +0530

    firast

--- STRING/SubSequence.java
+++ STRING/SubSequence.java
@@ -1,26 +1,26 @@
  class SubSequence {
-    static void SubSequence(String str1 , String str2){
-        for(int i=0; i<str1.length(); i++){
-            for(int j=0;j<str2.length(); j++){
-
-            
-            
-            if (str1.charAt(i) == str2.charAt(j)) {
+    static boolean  SubSequence(String str1 , String str2){
+        int i=0;
+        int j=0;
+        while(i<str1.length() && j<str2.length()){
+            if(str1.charAt(i) == str2.charAt(j)){
                 j++;
-            
-                
+
             }
-            else if(j == str2.length()){
-                System.out.println(str2 + " is a subsequence of " + str1 );
-            }
-           
+            i++;
+
         }
+        return j == str2.length();
     }
-}
     public static void main(String[] args){
         String str1 = "ABCDE";
         String str2 = "ABCD";
-        SubSequence(str1,str2);
+        if(SubSequence(str1,str2)){
+            System.out.println("Subsequence");
+        }
+        else{
+            System.out.println("Not a Subsequence");
+        }
 
     }
     
